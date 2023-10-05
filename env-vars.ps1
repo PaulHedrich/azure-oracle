42548cf5144d67a6c7b8c1f0750127d5d1eb2963
PaulHedrich <90277286+PaulHedrich@users.noreply.github.com>
2023-10-05 15:20:46 -0400

    Create env-vars.ps1

---
+++ env-vars.ps1
@@ -0,0 +1,14 @@
+### Authentication details
+$env:TF_VAR_tenant_id="22d9849c-79e1-45a0-8e75-2c410dd90fb3"
+
+$env:TF_VAR_subscription_id="510d7956-2baa-4b11-a385-3e2f39b6c97c"
+
+$env:TF_VAR_client_id="479026b0-c0ab-4c84-a93f-6ef741f80efe"
+
+$env:TF_VAR_client_secret="58f97b29-f688-426b-8d32-30bccc8e78f7"
+
+### Environment
+$env:TF_VAR_environment = "Corporate"
+
+### Region
+$env:TF_VAR_deployment_location="East US2"
